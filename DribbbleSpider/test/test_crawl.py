64ad06d616bcf20b3a9d407c8c78c204d6964b67
Jason Liang <jjjasonliang@gmail.com>
2020-05-08 18:17:38 +0800

    grab media files and related info

--- DribbbleSpider/test/test_crawl.py
+++ DribbbleSpider/test/test_crawl.py
@@ -9,15 +9,16 @@ headers = {
 }
 page = 1
 target_url = f"https://dribbble.com/?page={page}"
-
+categories = 'animation'
+target2 = f"https://dribbble.com/shots/{categories}/?page={page}"
 # xpath: //*[@class="group shot-thumbnail shot-thumbnail-with-hover-overlay"]/div/a
 
 
 def grab_design_links(url, headers):
-    res = requests.get(target_url, headers=headers)
+    res = requests.get(url, headers=headers)
     source = clean_html(res.content.decode(res.encoding))
     tree = etree.HTML(source)
-    link_path = "//div[@id = 'main']//li[contains(@class, 'shot-thumbnail-with-hover-overlay')]/div//a[contains(@class,'dribbble-link')]"
+    link_path = "//div[@id = 'main']//a[contains(@class,'dribbble-link')]"
     link_nodes = tree.xpath(link_path)
     url = []
     for a in link_nodes:
@@ -27,6 +28,6 @@ def grab_design_links(url, headers):
     return url
 
 
-links = grab_design_links(target_url, headers)
+links = grab_design_links(target2, headers)
 print(len(links))
 print(links)
